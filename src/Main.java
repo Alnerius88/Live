55cb1d3d0d189726968af059875093ffbaec8830
Alexander <alnerius88@yandex.ru>
2023-05-19 14:26:44 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phrase = "Коту скоро сорок суток.";
+        System.out.println(phrase);
+        phrase = phrase.toLowerCase();
+        phrase = phrase.replace(" ","");
+        phrase = phrase.replace(".","");
+        char[] lettersM1 = phrase.toCharArray();
+        System.out.println(lettersM1);
+        char[] lettersM2 = reverse(lettersM1);
+        System.out.println(lettersM2);
+        boolean palindrom = true;
+        if (palindrom) {
+            for (int i = 0; i < lettersM1.length; i++) {
+                if(lettersM1[i] != lettersM2[i]) {
+                    palindrom = false;}
+            }
+        }
+        if (palindrom) {
+            System.out.println("Эта фраза - палиндром.");
+        }else {
+            System.out.println("Эта фраза - не палиндром.");
+        }
+    }
+    static char[] reverse(char[] lettersM) {
+        char [] newLM = new char[lettersM.length];
+        for (int i = 0; i < lettersM.length; i++) {
+            newLM[lettersM.length - 1 - i] = lettersM[i];
+        }
+        return newLM;
     }
 }
